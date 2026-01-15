827f47e50780296804bdd9172d5f131ffccc0a1b
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2026-01-15 10:07:43 -0600

    Create pico_chatter.py

---
+++ pico_chatter.py
@@ -0,0 +1,15 @@
+import sys
+from time import time, ticks_ms, ticks_diff
+import select
+from machine import Pin
+from distance_sensor import DistanceSensor
+
+# SETUP
+pass
+
+# LOOP
+while True:
+    # Transmit data (TX)
+    pass
+    # Receive data (RX)
+    pass
