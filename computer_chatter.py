b36c5f90a1b67f46b7c3c5f1dcfb1b04d62e0fc9
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2026-01-15 10:10:03 -0600

    Create computer_chatter.py

---
+++ computer_chatter.py
@@ -0,0 +1,17 @@
+import serial
+from time import time, sleep
+
+# SETUP
+pass
+
+# LOOP
+try:
+    while True:
+        # Transmit data (TX)
+        pass
+        if usb_messenger.in_waiting > 0:
+        # Receive data (RX)
+                pass
+
+except KeyboardInterrupt:
+    pass
